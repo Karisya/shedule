d84e200adf725f7f57ce28a3b88278bff68a63cd
Karisya <karina-romanova-05@bk.ru>
2025-09-12 17:11:33 +0300

    connecting lesson data

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,36 +3,85 @@ import React from "react"
 
 const { TextArea } = Input;
 
+interface ScheduleEvent {
+  id: string;
+  title: string;
+  day: string;
+  slot: string;
+  teacher:string;
+  room:string;
+  type:string;
+  subject:string;
+  comment:string;
+}
+
 interface ModalWindowProps{
   day?:string;
   slot?:string;
-  onClose:()=>void
+  onClose:()=>void;
+  subjects: string[];
+  teachers:string[];
+  type:string[];
+  rooms:string[];
+  onSave:(event: Omit<ScheduleEvent, "id">) => void;
 }
 
-const ModalWindow:React.FC<ModalWindowProps>=({day, slot, onClose})=>{
 
-    return(
+
+const ModalWindow:React.FC<ModalWindowProps>=({day, slot, onClose,subjects, teachers, rooms, type, onSave})=>{
+  
+  const [form] = Form.useForm();
+  const handleOk= async ()=>{
+    const values= await form.validateFields();
+    onSave({...values, day, slot})
+    onClose();
+  }
+
+  return(
     <>
     <Modal 
       title={`Добавить занятие на ${day} ${slot}?`}
       onCancel={onClose}
       open={true}
+      onOk={handleOk}
       >
-      <p></p>
         <Form
         labelCol={{ span: 4 }}
         wrapperCol={{ span: 14 }}
         layout="horizontal"
         style={{ maxWidth: 600 }}
       >
-        <Form.Item label="Select">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
+        <Form.Item>
+          <Select
+            placeholder="Дисциплина"
+            options={subjects.map(sub=>({value:sub}))}
+            >
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Select
+            placeholder="Преподаватель"
+            options={teachers.map(teach=>({value:teach}))}
+            >
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Select
+            placeholder="Аудитория"
+            options={rooms.map(r=>({value:r}))}
+            >
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Select
+            placeholder="Тип занятия"
+            options={type.map(t=>({value:t}))}
+            >
           </Select>
         </Form.Item>
        
-        <Form.Item label="TextArea">
-          <TextArea rows={4} />
+        <Form.Item>
+          <TextArea placeholder="Комментарии" rows={4} />
         </Form.Item>
       </Form>
     </Modal>
--- src/components/Shedule/index.tsx
+++ src/components/Shedule/index.tsx
@@ -2,30 +2,45 @@ import React, { useState } from "react";
 import { Card } from "antd";
 import './style.css'
 import ModalWindow from "../Modal";
+import scheduleData from "../../data/shedule.json";
 
 interface ScheduleEvent {
   id: string;
   title: string;
   day: string;
   slot: string;
+  teacher:string;
+  room:string;
+  type:string;
+  subject:string;
+  comment:string;
 }
+
+
 
 const days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница","Суббота"];
 const slots = ["09:00-10:20", "10:30-11:50", "12:00-13:20", "13:50-15:10", "15:20-16:40","17:00-18:20" ];
 
-const events: ScheduleEvent[] = [
-  { id: "1", title: "Механика", day: "Понедельник", slot: "09:00-10:20" },
-];
 
 const ScheduleGrid: React.FC = () => {
 
+
   const [isOpen, setIsOpen]=useState(false)
   const [selectedElement, setSelectedElement]=useState<{day:string, slot:string}|null>(null)
+ const [events, setEvents] = useState<ScheduleEvent[]>(scheduleData.events);
 
   const handleClick=(day:string, slot:string)=>{
     setSelectedElement({day, slot})
     setIsOpen(true)
   }
+
+   const handleSave = (newEvent: Omit<ScheduleEvent, "id">) => {
+    setEvents((prev) => [
+      ...prev,
+      { id: String(prev.length + 1), ...newEvent }
+    ]);
+  };
+
 
 
   return (
@@ -59,6 +74,11 @@ const ScheduleGrid: React.FC = () => {
      day={selectedElement?.day}
      slot={selectedElement?.slot}
      onClose={() => setIsOpen(false)}
+     subjects={scheduleData?.subjects}
+     teachers={scheduleData.teachers}
+     rooms={scheduleData.rooms}
+     type={scheduleData.type}
+     onSave={handleSave}
      /> }
     </div>
   );
